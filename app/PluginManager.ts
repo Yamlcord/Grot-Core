94f4d69b5d4d77a0908c4234ea379512391cb0bc
CoconutDev13 <bakedbacon4@gmail.com>
2025-12-28 05:44:43 +0200

    Implemented service manager

--- app/PluginManager.ts
+++ app/PluginManager.ts
@@ -2,49 +2,51 @@ import { GrotCore } from ".";
 import { Plugin } from "./types";
 
 export class PluginManager {
-    private plugins: Array<Plugin>;
-    private core: GrotCore;
+  private plugins: Array<Plugin>;
+  private core: GrotCore;
 
-    public constructor(core: GrotCore) {
-        this.core = core;
-        this.plugins = new Array<Plugin>();
+  public constructor(core: GrotCore) {
+    this.core = core;
+    this.plugins = new Array<Plugin>();
+  }
+
+  registerPlugin(plugin: Plugin) {
+    const requiredIntents = plugin.requiredIntents;
+
+    for (const requiredIntent of requiredIntents || []) {
+      this.core.addIntent(requiredIntent);
     }
 
-    registerPlugin(plugin: Plugin) {
-        const requiredIntents = plugin.requiredIntents;
+    for (const dependency of plugin.dependencies || []) {
+      const dependencyPlugin = this.plugins.find(
+        (registeredPlugin) => registeredPlugin.name === dependency,
+      );
 
-        for (const requiredIntent of requiredIntents || []) {
-            this.core.addIntent(requiredIntent);
-        }
-
-        for (const dependency of plugin.dependencies || []) {
-            const dependencyPlugin = this.plugins.find(
-                (registeredPlugin) => registeredPlugin.name === dependency,
-            );
-
-            if (!dependencyPlugin) {
-                return new Error(
-                    `${dependency} coudln't be found but it's required for ${plugin.name}`,
-                );
-            }
-        }
-
-        console.log(`✅ Plugin loaded: ${plugin.name}`);
-        this.plugins.push(plugin);
+      if (!dependencyPlugin) {
+        return new Error(
+          `${dependency} coudln't be found but it's required for ${plugin.name}`,
+        );
+      }
     }
 
-    migratePlugins() {
-        const database = this.core.getDatabaseManager();
+    console.log(`✅ Plugin loaded: ${plugin.name}`);
+    this.plugins.push(plugin);
+  }
 
-        for(const plugin of this.plugins) {
-            plugin.initialize(this.core)
-            database.migrate(plugin.name, plugin.migrationsPath)
-        }
-    }
+  migratePlugins() {
+    const database = this.core.getDatabaseManager();
 
-    initializePlugins() {
-        for(const plugin of this.plugins) {
-            plugin.initialize(this.core)
-        }
+    for (const plugin of this.plugins) {
+      database.migrate(plugin.name, plugin.migrationsPath);
     }
+  }
+
+  initializePlugins() {
+    for (const plugin of this.plugins) {
+      plugin.initialize(
+        this.core,
+        this.core.getScopedServiceProvider(plugin.name),
+      );
+    }
+  }
 }
